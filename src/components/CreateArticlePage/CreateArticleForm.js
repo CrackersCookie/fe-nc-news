4e994c80b91e250fb2611b7c388496891f1bd687
Mark Cooke <crackerscookie@yahoo.com>
2019-08-07 19:10:06 +0100

    refactoring create article page components

--- src/components/CreateArticlePage/CreateArticleForm.js
+++ src/components/CreateArticlePage/CreateArticleForm.js
@@ -1,59 +1,85 @@
-import React, { Component } from 'react';
-import styles from './CreateArticleForm.module.css';
-import * as api from "../../api"
-import { navigate } from '@reach/router';
+import React, { Component } from "react";
+import styles from "./CreateArticleForm.module.css";
+import * as api from "../../api";
+import { navigate } from "@reach/router";
 
 class CreateArticleForm extends Component {
   state = {
-    title: '',
-    body: '',
-    topic: 'coding',
+    title: "",
+    body: "",
+    topic: "coding",
     error: null,
     bodyCharacterLimit: 5000
-  }
+  };
 
   render() {
+    const { title, body, bodyCharacterLimit } = this.state;
+
     return (
       <article className={styles.article}>
         <form onSubmit={this.handleSubmit} className={styles.postForm}>
-          <input value={this.state.title} onChange={this.handleTextChange} type="text" name="title" placeholder="Title..." required />
-          <select value={"topic"} onChange={this.handleOptionChange} className={styles.select}>
+          <input
+            value={title}
+            onChange={this.handleTextChange}
+            type="text"
+            name="title"
+            placeholder="Title..."
+            required
+          />
+          <select
+            value={"topic"}
+            onChange={this.handleOptionChange}
+            className={styles.select}
+          >
             <option value="coding">coding</option>
             <option value="football">football</option>
             <option value="cooking">cooking</option>
           </select>
-          <textarea value={this.state.body} onChange={this.handleTextChange} rows="20" cols="20" name="body" placeholder="Write your article here..." required></textarea>
-          <p className={styles.characters}>characters remaing: {this.state.bodyCharacterLimit - this.state.body.length}</p>
-          <input type="submit" value="Submit"></input>
+          <textarea
+            value={body}
+            onChange={this.handleTextChange}
+            rows="20"
+            cols="20"
+            name="body"
+            placeholder="Write your article here..."
+            required
+          />
+          <p className={styles.characters}>
+            characters remaing: {bodyCharacterLimit - body.length}
+          </p>
+          <input type="submit" value="Submit" />
         </form>
       </article>
     );
   }
 
   handleTextChange = ({ target: { name, value } }) => {
-    const { bodyCharacterLimit } = this.state
-    const characterLimitCheck = name === 'body' && value.length <= bodyCharacterLimit
-    if (name === 'title' || characterLimitCheck) {
-      this.setState({ [name]: value })
+    const { bodyCharacterLimit } = this.state;
+    const characterLimitCheck =
+      name === "body" && value.length <= bodyCharacterLimit;
+    if (name === "title" || characterLimitCheck) {
+      this.setState({ [name]: value });
     }
-  }
+  };
 
-  handleOptionChange = ({ target: { name, value } }) => {
-    this.setState({ topic: value })
-  }
+  handleOptionChange = ({ target: { value } }) => {
+    this.setState({ topic: value });
+  };
 
-  handleSubmit = (e) => {
-    const { title, body, topic } = this.state
-    const author = this.props.username
-    e.preventDefault()
-    api.postArticle({ author, title, body, topic }).then(({ article_id }) => {
-      navigate(`/articles/${article_id}`)
-    }).catch(({ response }) => {
-      const error = { status: response.status, msg: response.data.msg }
-      this.setState({ error, isLoading: false })
-    })
-
-  }
+  handleSubmit = e => {
+    const { title, body, topic } = this.state;
+    const author = this.props.username;
+    e.preventDefault();
+    api
+      .postArticle({ author, title, body, topic })
+      .then(({ article_id }) => {
+        navigate(`/articles/${article_id}`);
+      })
+      .catch(({ response }) => {
+        const error = { status: response.status, msg: response.data.msg };
+        this.setState({ error, isLoading: false });
+      });
+  };
 }
 
 export default CreateArticleForm;
--- src/components/CreateArticlePage/CreateArticlePage.js
+++ src/components/CreateArticlePage/CreateArticlePage.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import CreateArticleForm from './CreateArticleForm';
+import React from "react";
+import CreateArticleForm from "./CreateArticleForm";
 
 const CreateArticlePage = ({ username }) => {
-
   return (
     <section>
       <h1>Create Article</h1>
       <CreateArticleForm username={username} />
     </section>
   );
-}
+};
 
 export default CreateArticlePage;
